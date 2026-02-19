6cf9092eeb69fe9c760d15bccf28f96420e508e4
Sayandip-Jana-1018 <jsupriti3434@gmail.com>
2026-02-19 13:46:07 +0530

    Sum EJS

--- nodejs/4.js
+++ nodejs/4.js
@@ -2,17 +2,17 @@ import express from 'express'
 const app = express()
 app.listen(8080, () => console.log("Server Started"))
 
-app.get("/:id/:email", (req, res) => {
-    console.log(req.url)
-    console.log(req.params)
-    res.send(req.params.id + req.params.email);
-});
+// app.get("/:id/:email", (req, res) => {
+//     console.log(req.url)
+//     console.log(req.params)
+//     res.send(req.params.id + req.params.email);
+// });
 
-app.get("/id/:id/email/:email", (req, res) => {
-    console.log(req.url)
-    console.log(req.params)
-    res.send(req.params.id + req.params.email);
-});
+// app.get("/id/:id/email/:email", (req, res) => {
+//     console.log(req.url)
+//     console.log(req.params)
+//     res.send(req.params.id + req.params.email);
+// });
 
 app.get("/home", (req, res) => {
     res.send("Hello World")
@@ -24,6 +24,17 @@ app.get("/:num1/:num2", (req, res) => {
     const num2 = Number(req.params.num2);
 
     const sum = num1 + num2;
+
+    res.send(`Sum = ${sum}`);
+});
+
+app.get("/x/:a/y/:b/z/:c", (req, res) => {
+
+    const a = Number(req.params.a);
+    const b = Number(req.params.b);
+    const c = Number(req.params.c);
+
+    const sum = a + b + c;
 
     res.send(`Sum = ${sum}`);
 });
